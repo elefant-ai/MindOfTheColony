b75bfc8f133558ba393c2e6d29d580236dc53ab8
Night_Hunter <samuelhuntnz@gmail.com>
2026-01-27 02:11:59 +1300

    cleanup

--- src/main/java/com/goodbird/mindofthecolony/mixin/IExtendedCitizenData.java
+++
@@ -1,7 +0,0 @@
-package com.goodbird.mindofthecolony.mixin;
-
-import net.minecraft.nbt.CompoundTag;
-
-public interface IExtendedCitizenData {
-    CompoundTag getLoadedConversationHistoryNBT();
-}
